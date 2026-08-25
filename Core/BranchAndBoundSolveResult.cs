c1e3ec52c3a10c9ad54a03a85db7e32a6816d71c
Cimannin <viljoensteenkamp2004@gmail.com>
2026-08-25 08:40:34 +0200

    This push includes KNapsack and Branch and bound Simplex. I've tested it with separate text files, some optimal and some unbounded or infeasible.

---
+++ Core/BranchAndBoundSolveResult.cs
@@ -0,0 +1,25 @@
+﻿using LPR381Solver.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LPR381Solver.Models;
+
+namespace LPR381Solver.Core
+{
+    public class BranchAndBoundSolveResult
+    {
+        public SolveStatus Status { get; set; }
+        public List<BranchAndBoundNode> ExploredNodes { get; set; }
+        public BranchAndBoundNode BestNode { get; set; }
+
+        public double[] OriginalVariableValues { get; set; }
+        public double ObjectiveValue { get; set; }
+
+        public BranchAndBoundSolveResult()
+        {
+            ExploredNodes = new List<BranchAndBoundNode>();
+        }
+    }
+}
---
+++ Core/KnapsackSolveResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LPR381Solver.Models;
+
+namespace LPR381Solver.Core
+{
+    public class KnapsackSolveResult
+    {
+        public SolveStatus Status { get; set; }
+
+        public List<KnapsackItem> Items { get; set; }
+        public List<KnapsackNode> ExploredNodes { get; set; }
+        public KnapsackNode BestNode { get; set; }
+
+        public double[] OriginalVariableValues { get; set; }
+        public double ObjectiveValue { get; set; }
+        public double Capacity { get; set; }
+
+        public KnapsackSolveResult()
+        {
+            Items = new List<KnapsackItem>();
+            ExploredNodes = new List<KnapsackNode>();
+        }
+    }
+}
